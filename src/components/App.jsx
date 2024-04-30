e65255b36246e1b5e93338216f266679b10e2a9c
Tanushree227 <tanushreedas813@gmail.com>
2024-05-01 00:30:24 +0530

    mapping data to functions

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,23 @@
-import React from 'react'
-import Header from './Header'
-import Footer from './Footer'
-import Note from './Note'
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import notes from "../notes";
 
 export default function App() {
   return (
     <div>
       <Header />
-      <Note title="a title" content="a content" />
+      {notes.map((noteItem) => {
+        return (
+          <Note
+            key={noteItem.key}
+            title={noteItem.title}
+            content={noteItem.content}
+          />
+        );
+      })}
       <Footer />
     </div>
-  )
+  );
 }
---
+++ src/notes.js
@@ -0,0 +1,24 @@
+const notes = [
+    {
+        key: 1,
+        title: "Digital Marketing",
+        content: "Marketing Product or Businesses Online"
+    },
+    {
+        key: 2,
+        title: "UI/UX",
+        content: "Visual appealing of a website"
+    },
+    {
+        key: 3,
+        title: "Data Analytics",
+        content: "To draw out meaningful insights from a raw data."
+    },
+    {
+        key: 4,
+        title: "Software Development",
+        content: "Developing a applications using trending technologies"
+    }
+]
+
+export default notes;
