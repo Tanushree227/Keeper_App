0358a96b8e4e7b798805c4398307550a5cc704d3
Tanushree227 <tanushreedas813@gmail.com>
2024-05-01 00:03:31 +0530

    displaying current year dynamically

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,6 +3,6 @@ import React from 'react'
 export default function Footer() {
     const year = new Date().getFullYear();
   return (
-    <p>Copyright ©️ {year}</p>
+    <footer><p>Copyright ©️ {year}</p></footer>
   )
 }
