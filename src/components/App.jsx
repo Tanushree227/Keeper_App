f9554545feadecc272209224fc7c354567eff021
Tanushree227 <tanushreedas813@gmail.com>
2024-05-01 17:56:51 +0530

    major changes has implemented

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -15,14 +15,26 @@ export default function App() {
     })
   }
 
+  function deleteNote(id)
+  {
+    setNotes(prevNotes => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      });
+    })
+  }
+
   return (
     <div>
       <Header />
       <CreateArea onAdd={addNote} />
-      {notes.map((noteItem) => {
+      {notes.map((noteItem, index) => {
         return <Note
-          title={noteItem.title}
-          content={noteItem.content}
+          key = {index}
+          id = {index}
+          title = {noteItem.title}
+          content = {noteItem.content}
+          onDelete = {deleteNote}
          />
       })}
       <Footer />
